b955b5f73c4664f16c000cbb514f221c5c60237b
Sankalp Sharma <119119156+RIVAL231@users.noreply.github.com>
2024-06-12 03:27:20 +0530

    Update data.js

--- src/Components/data.js
+++ src/Components/data.js
@@ -6,22 +6,22 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "https://images.unsplash.com/photo-1618278942403-e973260cc425?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TW91bnQlMjBGdWppfGVufDB8fDB8fHww"
     },
     {
         title: "Sydney Opera House",
         location: "AUSTRALIA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/wdfZsaGMV1rUvgzUA",
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+        imageUrl: "https://images.unsplash.com/photo-1549180030-48bf079fb38a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFN5ZG5leSUyME9wZXJhJTIwSG91c2V8ZW58MHx8MHx8fDA%3D"
     },
     {title: "Geirangerfjord",
         location: "NORWAY",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/oBRasvXkbDqi7RsD8",
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"}
+        imageUrl: "https://plus.unsplash.com/premium_photo-1677343210638-5d3ce6ddbf85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8R2VpcmFuZ2VyZmpvcmR8ZW58MHx8MHx8fDA%3D"}
 ]
